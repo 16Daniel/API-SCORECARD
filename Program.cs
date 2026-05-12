0f30cdd10a40fa540158b6739b710092068a97dc
Carlos R <carlos.r@operamx.com>
2026-05-12 12:26:28 -0600

    configuracion de variables de entorno y apikey

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 using DashboardApi.Funciones;
 using DashboardApi.Mail;
+using DashboardApi.Middlewares;
 using DashboardApi.ModelsBD1;
 using DashboardApi.ModelsBD2;
 using DashboardApi.ModelsDashboard;
 using DashboardApi.ModelsDBRebel;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using Quartz;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,69 +40,49 @@ builder.Services.AddScoped<MailC>();
 builder.Services.AddScoped<Funciones>();
 builder.Services.AddScoped<FuncionesBonos>();
 
-
-//// Configurar Quartz
-//builder.Services.AddQuartz(q =>
-//{
-//    // Just use the name of your job that you created in the Jobs folder.
-//    var jobKey = new JobKey("SendEmailJob");
-//    q.AddJob<MainJobs>(opts => opts.WithIdentity(jobKey));
-
-//    q.AddTrigger(opts => opts
-//        .ForJob(jobKey)
-//        .WithIdentity("SendEmailJob-trigger")
-//        .WithSimpleSchedule(x => x
-//            .WithIntervalInMinutes(30) // Intervalo de 30 minutos
-//            .RepeatForever()) // Se repite indefinidamente
-//    );
-//});
-
-
-//builder.Services.AddQuartz(q =>
-//{
-//    // Just use the name of your job that you created in the Jobs folder.
-//    var jobKey = new JobKey("SendEmailJobMermas");
-//    q.AddJob<MainJobs>(opts => opts.WithIdentity(jobKey));
-
-//    q.AddTrigger(opts => opts
-//        .ForJob(jobKey)
-//        .WithIdentity("SendEmailJobMermas-trigger")
-//        //This Cron interval can be described as "run every minute" (when second is zero)  
-//        .WithCronSchedule("0 42 8 * * ?")
-//    );
-//});
-
-//builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-app.UseCors();
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-app.UseSwaggerUI(c =>
+builder.Services.AddSwaggerGen(options =>
 {
-    app.UseSwagger().UseDeveloperExceptionPage();
-#if DEBUG
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API DASHBOARD v1");
-#else
-    c.SwaggerEndpoint("/back/api_planeacion/swagger/v1/swagger.json", "API_PEDIDOS v1");
-#endif
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "RW DashboardApi",
+        Version = "v0.0.1",
+        Description = "API para administración del dashboard"
+    });
+
+    options.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "API Key requerida en el header: x-api-key",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "x-api-key",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                }
+            },
+            new string[] {}
+        }
+    });
 });
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
-
+app.UseCors();
+app.UseMiddleware<ApiKeyMiddleware>();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
